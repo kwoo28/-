a0011b6ee88bdd53fcc50e00538855fc688c8d32
kwoo28 <148550522+kwoo28@users.noreply.github.com>
2024-01-15 17:13:36 +0900

    Feat: SwaggerConfig 추가

---
+++ src/main/java/projectboard/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package projectboard.config;
+
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+//https://velog.io/@kjgi73k/Springboot3%EC%97%90-Swagger3%EC%A0%81%EC%9A%A9%ED%95%98%EA%B8%B0
+public class SwaggerConfig {
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
