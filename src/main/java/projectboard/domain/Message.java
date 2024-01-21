d4e039d6767a2da210456029d53e7489b983db7b
kwoo28 <148550522+kwoo28@users.noreply.github.com>
2024-01-21 15:54:50 +0900

    [Modify] 보낸편지함, 받는편지함 -> 편지함 통합

--- src/main/java/projectboard/domain/Message.java
+++ src/main/java/projectboard/domain/Message.java
@@ -10,26 +10,30 @@ import java.sql.Timestamp;
 
 @Getter @Setter
 @NoArgsConstructor
-public class RecvMessage {
+public class Message {
     @ApiModelProperty(example = "쪽지 고유 번호")
     private Long id;
     @ApiModelProperty(example = "쪽지 보낸 유저 고유 번호")
-    private Long sendUserId;
+    private Long sendId;
     @ApiModelProperty(example = "쪽지 받는 유저 고유 번호")
-    private Long recvUserId;
+    private Long recvId;
     @ApiModelProperty(example = "쪽지 보낸 일자")
     private Timestamp sendAt;
     @ApiModelProperty(example = "쪽지 읽은 일자")
-    private Timestamp receiveAt;
+    private Timestamp recvAt;
+    @ApiModelProperty(example = "쪽지 확인 여부(0=읽지않음, 1=읽음)")
+    private int checked;
     @ApiModelProperty(example = "쪽지 제목")
     private String title;
     @ApiModelProperty(example = "쪽지 내용")
     private String content;
 
     @Builder
-    public RecvMessage(Long sendUserId, Long recvUserId, String title, String content) {
-        this.sendUserId = sendUserId;
-        this.recvUserId = recvUserId;
+    public Message(Long sendId, Long recvId, Timestamp sendAt, Timestamp recvAt, String title, String content) {
+        this.sendId = sendId;
+        this.recvId = recvId;
+        this.sendAt = sendAt;
+        this.recvAt = recvAt;
         this.title = title;
         this.content = content;
     }
